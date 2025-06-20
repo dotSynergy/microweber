66ca27dcc02e68ae5d1426800ca0ae1f2cbf655a
Peter Ivanov <peter@microweber.com>
2025-06-20 17:54:12 +0300

    update

--- packages/frontend-assets/resources/assets/ui/components/RightSidebar/ToolsButtons.vue
+++ packages/frontend-assets/resources/assets/ui/components/RightSidebar/ToolsButtons.vue
@@ -1,5 +1,19 @@
 <template>
     <ul class="d-grid gap-3 list-unstyled p-3 pb-0">
+
+
+        <li>
+            <a class="mw-admin-action-links mw-adm-liveedit-tabs" v-on:click="openSetupWizard()">
+                <svg fill="currentColor" class="mb-1 me-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
+                    <path d="M480-400 40-640l440-240 440 240-440 240Zm0 160L63-467l84-46 333 182 333-182 84 46-417 227Zm0 160L63-307l84-46 333 182 333-182 84 46L480-80Zm0-411 273-149-273-149-273 149 273 149Zm0-149Z"/>
+                </svg>
+                <Lang>Setup wizard</Lang>
+            </a>
+        </li>
+
+
+
+
         <li>
             <a class="mw-admin-action-links mw-adm-liveedit-tabs" v-on:click="showCodeEditor()">
                 <svg fill="currentColor" class="mb-1 me-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
@@ -19,14 +33,6 @@
             </a>
         </li>
 
-        <li>
-            <a class="mw-admin-action-links mw-adm-liveedit-tabs" v-on:click="openSetupWizard()">
-                <svg fill="currentColor" class="mb-1 me-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
-                <path d="M480-400 40-640l440-240 440 240-440 240Zm0 160L63-467l84-46 333 182 333-182 84 46-417 227Zm0 160L63-307l84-46 333 182 333-182 84 46L480-80Zm0-411 273-149-273-149-273 149 273 149Zm0-149Z"/>
-            </svg>
-                <Lang>Setup wizard</Lang>
-            </a>
-        </li>
 
         <li>
             <a class="mw-admin-action-links mw-adm-liveedit-tabs" v-on:click="openContentResetContent()">

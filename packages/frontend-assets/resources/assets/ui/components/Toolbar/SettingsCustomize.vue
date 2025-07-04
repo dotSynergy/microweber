fa74d41d1b1c52f2e71e1c2f68f8b64db6ee53d2
Nikolay <rachkuw@gmail.com>
2025-07-04 13:22:58 +0300

    Update SettingsCustomize.vue

--- packages/frontend-assets/resources/assets/ui/components/Toolbar/SettingsCustomize.vue
+++ packages/frontend-assets/resources/assets/ui/components/Toolbar/SettingsCustomize.vue
@@ -135,19 +135,17 @@
         <div  style="margin-top:auto;">
             <Transition name="advanced">
                 <div  v-if=" advanced" style="width: 100%;margin-bottom: 20px">
+                    <div class="mw-admin-action-links mw-adm-liveedit-tabs" v-on:click="handleLayers()"  :class="{'live-edit-right-sidebar-active': !!layers }">
+                        <v-tooltip activator="parent" location="start"><Lang>Layers</Lang></v-tooltip>
+
+                        <svg fill="currentColor" class="mb-1 me-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
+                            <path d="M480-400 40-640l440-240 440 240-440 240Zm0 160L63-467l84-46 333 182 333-182 84 46-417 227Zm0 160L63-307l84-46 333 182 333-182 84 46L480-80Zm0-411 273-149-273-149-273 149 273 149Zm0-149Z"/>
+                        </svg>
+                        <Lang>Layers</Lang>
+                    </div>
                     <ToolsButtons></ToolsButtons>
                 </div>
             </Transition>
-
-
-            <div class="mw-admin-action-links mw-adm-liveedit-tabs" v-on:click="handleLayers()"  :class="{'live-edit-right-sidebar-active': !!layers }">
-                <v-tooltip activator="parent" location="start"><Lang>Layers</Lang></v-tooltip>
-
-                <svg fill="currentColor" class="mb-1 me-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
-                    <path d="M480-400 40-640l440-240 440 240-440 240Zm0 160L63-467l84-46 333 182 333-182 84 46-417 227Zm0 160L63-307l84-46 333 182 333-182 84 46L480-80Zm0-411 273-149-273-149-273 149 273 149Zm0-149Z"/>
-                </svg>
-                <Lang>Layers</Lang>
-            </div>
 
 
             <div v-on:click="handleAdvanced()" :class="{'live-edit-right-sidebar-active': buttonIsActiveQuickEdit }"

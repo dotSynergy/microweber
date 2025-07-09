60df1c547485d49bd3039a0b8851470354da2922
Nikolay <rachkuw@gmail.com>
2025-07-09 16:58:47 +0300

    Update CurrentLayoutSettingsButtons.vue

--- packages/frontend-assets/resources/assets/ui/components/Toolbar/CurrentLayoutSettingsButtons.vue
+++ packages/frontend-assets/resources/assets/ui/components/Toolbar/CurrentLayoutSettingsButtons.vue
@@ -13,7 +13,9 @@
                 <v-tooltip activator="parent" location="start">
                     Insert Module
                 </v-tooltip>
-                <span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M352-120H200q-33 0-56.5-23.5T120-200v-152q48 0 84-30.5t36-77.5q0-47-36-77.5T120-568v-152q0-33 23.5-56.5T200-800h160q0-42 29-71t71-29q42 0 71 29t29 71h160q33 0 56.5 23.5T800-720v160q42 0 71 29t29 71q0 42-29 71t-71 29v160q0 33-23.5 56.5T720-120H568q0-50-31.5-85T460-240q-45 0-76.5 35T352-120Zm-152-80h85q24-66 77-93t98-27q45 0 98 27t77 93h85v-240h80q8 0 14-6t6-14q0-8-6-14t-14-6h-80v-240H480v-80q0-8-6-14t-14-6q-8 0-14 6t-6 14v80H200v88q54 20 87 67t33 105q0 57-33 104t-87 68v88Zm260-260Z"/></svg></span>
+                <span>
+                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-120v-320H120v-80h320v-320h80v320h320v80H520v320h-80Z"/></svg>
+                </span>
             </div>
         </div>
 
@@ -43,8 +45,8 @@
 .current-layout-modules {
     display: flex;
     flex-direction: column;
-    gap: 8px;
-    padding: 10px 0;
+    gap: 15px;
+    padding: 15px 0;
     border-bottom: 1px solid rgba(255, 255, 255, 0.1);
     margin-bottom: 10px;
 }
@@ -69,7 +71,7 @@
 .modules-buttons {
     display: flex;
     flex-direction: column;
-    gap: 8px;
+    gap: 15px;
     align-items: center;
 }
 

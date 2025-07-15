e481e1f15833276a365b016609f7366956c6359d
Alex <alexander.raikov@gmail.com>
2025-07-15 17:27:10 +0300

    live edit

--- packages/frontend-assets/resources/assets/ui/components/RightSidebar/RightSidebar.vue
+++ packages/frontend-assets/resources/assets/ui/components/RightSidebar/RightSidebar.vue
@@ -77,9 +77,13 @@ OLD iframe is commented out, use the new TemplateSettings component instead
     left: 250px;
 }
 
+body:has(.fi-modal.fi-modal-open) #live-edit-frame-holder,
 body:has(#mw-setup-wizard-dialog.active) #live-edit-frame-holder,
 .live-edit-gui-editor-opened #live-edit-frame-holder {
     right: calc(var(--sidebar-end-size) + 50px);
+}
+.fi-modal.fi-modal-open .mw-module-settings-live-edit-modal{
+    max-width: calc(var(--sidebar-end-size) + 50px);
 }
 </style>
 

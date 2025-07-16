3abd6067ff31b0d66a5c3afb12a745fcdf961e86
Peter Ivanov <peter@microweber.com>
2025-07-16 12:42:48 +0300

    update

---
+++ packages/frontend-assets/resources/assets/ui/components/Toolbar/CurrentLayoutSettingsButtonsTeleport.vue
@@ -0,0 +1,27 @@
+<template>
+    <Teleport :to="teleportTarget" defer>
+
+
+        <CurrentLayoutSettingsButtons></CurrentLayoutSettingsButtons>
+
+    </Teleport>
+</template>
+
+
+<script>
+
+import CurrentLayoutSettingsButtons from './CurrentLayoutSettingsButtons.vue';
+
+export default {
+    components: {
+        CurrentLayoutSettingsButtons,
+    },
+    props: {
+        teleportTarget: {
+            type: String,
+            default: '#current-layout-settings-teleport-widget-content'
+        }
+
+    }
+};
+</script>

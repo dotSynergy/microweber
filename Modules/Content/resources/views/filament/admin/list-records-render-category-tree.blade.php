8c22b2fd27aab3c0d82545de9b1e4301013e8615
Peter Ivanov <peter@microweber.com>
2025-07-22 16:29:26 +0300

    update

--- Modules/Content/resources/views/filament/admin/list-records-render-category-tree.blade.php
+++ Modules/Content/resources/views/filament/admin/list-records-render-category-tree.blade.php
@@ -19,34 +19,61 @@
 
     document.addEventListener('livewire:initialized', () => {
 
-        let treeControllBox = false, pagesTree ;
+        let treeControllBox = false, pagesTree;
 
         Livewire.on('showCategoriesSelectorPanel', async () => {
-            if(!treeControllBox) {
+            if (!treeControllBox) {
                 const id = mw.id();
                 treeControllBox = new mw.controlBox({
                     content: `<div id="${id}" style="min-width: 250px;padding: 50px 0 0 30px"></div>`,
-                    position:  'left',
+                    position: 'left',
                     id: `${id}`,
                     closeButton: true
                 });
                 treeControllBox.show()
-                pagesTree = await mw.widget.tree(`#${id}`, {selectableNodes: false, selectable: false, singleSelect: true,}, 'tree');
+                pagesTree = await mw.widget.tree(`#${id}`, {
+                    selectableNodes: false,
+                    selectable: false,
+                    singleSelect: true,
+                }, 'tree');
 
                 pagesTree.tree.on('selectionChange', e => {
                     const result = pagesTree.tree.getSelected();
+                    let module = document.querySelector('.fi-page');
 
-                    console.log(result);
+                    // var selected = tree.getSelected();
+                    if (result) {
+
+                        //get only categories
+
+                        var cats = [];
+                        result.forEach(function (item) {
+                            if (item.type === 'category') {
+                                cats.push(item);
+                            }
+                        });
+                        if( cats.length === 0){
+                            const component = Livewire.find(module.getAttribute('wire:id'));
+                            if(component) {
+                                component.set('tableFilters.category_id.value', '');
+                                component.$refresh();
+                            }
+                            return;
+                        }
+
+                        const component = Livewire.find(module.getAttribute('wire:id'));
+                        if(component) {
+                            component.set('tableFilters.category_id.value', cats[0].id);
+                            component.$refresh();
+                        }
+
+                    }
+
                 })
 
             } else {
                 treeControllBox.toggle();
             }
-
-            console.log(treeControllBox);
-            console.log(pagesTree);
-
-
 
 
         });

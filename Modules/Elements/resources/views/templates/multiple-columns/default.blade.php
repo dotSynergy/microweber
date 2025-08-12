a788ba7936deaa95e89eb53c2568955ee5fef065
Alex <alexander.raikov@gmail.com>
2025-08-12 16:51:47 +0300

    live edit

--- Modules/Elements/resources/views/templates/multiple-columns/default.blade.php
+++ Modules/Elements/resources/views/templates/multiple-columns/default.blade.php
@@ -2,11 +2,11 @@
     $cols = 3;
 @endphp
 
-<div class="mw-row no-edit" style="margin-block:0;padding: 15px 0;">
+<div class="mw-row no-select nodrop" style="margin-block:0;padding: 15px 0;">
     @for($i = 1; $i <= $cols; $i++)
-        <div class="mw-col" style="width: {{ 100/$cols }}%">
-            <div class="mw-col-container safe-mode element">
-                <div class="mw-empty-element element allow-drop allow-edit"></div>
+        <div class="mw-col cloneable allow-select" style="width: {{ 100/$cols }}%">
+            <div class="mw-col-container safe-mode element no-select">
+                <div class="mw-empty-element element allow-drop allow-edit allow-select"></div>
             </div>
         </div>
     @endfor

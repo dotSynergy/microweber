f191ce15805c1be7aeed1e5ecf92e2acf4cc9ce3
Peter Ivanov <peter@microweber.com>
2025-08-26 15:59:03 +0300

    update

--- Modules/Payment/Tests/Unit/PaymentProviderDriversTest.php
+++ Modules/Payment/Tests/Unit/PaymentProviderDriversTest.php
@@ -58,7 +58,7 @@ class PaymentProviderDriversTest extends TestCase
             ->assertHasNoActionErrors()
             ->assertNotified()
             ->assertHasNoErrors();
- 
+
         $this->assertTrue(PaymentProvider::where('name', 'Pay on delivery')->exists());
 
 
